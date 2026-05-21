ff8316a4d27c5e68a2e4ca8cdeb9cb4a84155d93
Devika <dech8707@gmail.com>
2026-05-21 23:07:16 +0530

    first Assignment

---
+++ index.js
@@ -0,0 +1,37 @@
+var studentName = "Devika";
+console.log(studentName);
+
+let age = 20;
+age = 25;
+console.log(age);
+
+const country = "India";
+console.log(country);
+
+let name = "Devika";
+let age2 = 20;
+let city = "Delhi";
+console.log(name, age2, city);
+
+let a=5;
+let b=7;
+let c=a+b;
+console.log(c);
+
+let str1 = "JavaScript";
+console.log(str1[0]);
+
+let str2 = "Programming";
+console.log(str2[10]);
+
+let str3 = "HELLO WORLD";
+console.log(str3.toLowerCase());
+
+let str4 = "javascript";
+console.log(str4.toUpperCase());
+
+let student = {
+        name: "devika",
+        course: "bca",
+    }
+console.log(student);
